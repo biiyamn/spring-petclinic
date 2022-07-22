716f981cb3c17a54372a0ced3942c65cfc0eecac
biiyamn <aymen.laabidi@gmail.com>
2022-07-22 11:01:17 +0100

    add ajp config

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -16,8 +16,13 @@
 
 package org.springframework.samples.petclinic;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 /**
  * PetClinic Spring Boot Application.
@@ -27,7 +32,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class PetClinicApplication {
+	@Value("${tomcat.ajp.port}")
+	int ajpPort;
 
+	@Value("${tomcat.ajp.remoteauthentication}")
+	String remoteAuthentication;
+
+	@Value("${tomcat.ajp.enabled}")
+	boolean tomcatAjpEnabled;
+
+	@Bean
+	public TomcatServletWebServerFactory servletContainer() {
+
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		if (tomcatAjpEnabled) {
+			Connector ajpConnector = new Connector("AJP/1.3");
+			ajpConnector.setPort(ajpPort);
+			ajpConnector.setSecure(false);
+			ajpConnector.setAllowTrace(false);
+			ajpConnector.setScheme("http");
+			((AbstractAjpProtocol) ajpConnector.getProtocolHandler()).setSecretRequired(false);
+			tomcat.addAdditionalTomcatConnectors(ajpConnector);
+		}
+
+		return tomcat;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
